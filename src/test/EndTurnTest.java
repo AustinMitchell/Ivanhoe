0f9308f477d4b7082a5425fdbab63c54119d9b33
Ahmedabdelghani <ahmed.abdelghani.101@gmail.com>
2016-02-28 22:53:25 -0500

    creating more rules engine tests

--- src/test/EndTurnTest.java
+++ src/test/EndTurnTest.java
@@ -4,14 +4,15 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import models.GameState;
 import models.Player;
 import rulesengine.RulesEngine;
+import rulesengine.Type;
 
 public class EndTurnTest {
-
 	Player player1;
 	Player player2;
 	Player player3;
@@ -20,54 +21,114 @@ public class EndTurnTest {
 	ArrayList<Player> players;
 	GameState game;
 	RulesEngine engine = new RulesEngine();
-	int purples;
 
 	
-	//@Before
+	@Before
 	public void setupPlayers() {
 		player1 = new Player("Ahmed");
-		player1.enterTournament();
 		player2 = new Player("Nick");
-		player2.enterTournament();
 		player3 = new Player("Austin");
-		player3.enterTournament();
 		player4 = new Player("Mike");
-		player4.enterTournament();
 		player5 = new Player("Malik");
-		player5.enterTournament();
+		
+	}
+	
+	@Test
+	public void twoPlayerEndTurnTest() {
+		players = new ArrayList();
+		players.add(player1);
+		players.add(player2);
+		game = new GameState(players);
+		
+		//ending turn after starting from index 0 (which is the current turn)
+		assertEquals("endTurn:1", engine.endTurn(game, "false"));
+		
+		//ending a turn with the withdrawal of index 1 (which is the current turn)
+		//since there are only 2 players, the winning player is at index 0
+		assertEquals("endTournament:0", engine.endTurn(game, "true"));
+	}
+	
+	
+	@Test
+	public void threePlayerEndTurnTest() {
+		players = new ArrayList();
+		players.add(player1);
+		players.add(player2);
+		players.add(player3);
+		game = new GameState(players);
+		
+		//ending turn after starting from index 0 (which is the current turn)
+		assertEquals("endTurn:1", engine.endTurn(game, "false"));
+		
+		//ending a turn with the withdrawal of index 1 (which is the current turn)
+		assertEquals("endTurn:2", engine.endTurn(game, "true"));
+		
+		//player at index 2 ended their turn normally
+		assertEquals("endTurn:0", engine.endTurn(game, "false"));
+		
+		//player at index 0 withdrew from the game leaving only 1 winner at index 2
+		assertEquals("endTournament:2", engine.endTurn(game, "true"));
+	}
+	
+	
+	@Test
+	public void fourPlayerEndTurnTest() {
+		players = new ArrayList();
+		players.add(player1);
+		players.add(player2);
+		players.add(player3);
+		players.add(player4);
+		game = new GameState(players);
+		
+		//ending turn after starting from index 0 (which is the current turn)
+		assertEquals("endTurn:1", engine.endTurn(game, "false"));
+		
+		//ending a turn with the withdrawal of index 1 (which is the current turn)
+		assertEquals("endTurn:2", engine.endTurn(game, "true"));
+		
+		//player at index 2 ended their turn normally
+		assertEquals("endTurn:3", engine.endTurn(game, "false"));
+		
+		//player at index 3 withdrew from the game
+		assertEquals("endTurn:0", engine.endTurn(game, "true"));
+		
+		//player at index 0 ends their turn normally
+		assertEquals("endTurn:2", engine.endTurn(game, "false"));
+		
+		//player at index 2 withdrew from the game leaving only 1 winner at index 0
+		assertEquals("endTournament:0", engine.endTurn(game, "true"));
+	}
+
+	
+	@Test
+	public void fivePlayerEndTurnTest() {
 		players = new ArrayList();
 		players.add(player1);
 		players.add(player2);
 		players.add(player3);
 		players.add(player4);
 		players.add(player5);
-		
 		game = new GameState(players);
 		
-		player1 = new Player("Ahmed");
-		player1.enterTournament();
-		player2 = new Player("Nick");
-		player2.enterTournament();
-		players = new ArrayList();
-		players.add(player1);
-		players.add(player2);
-		game = new GameState(players);
+		//ending turn after starting from index 0 (which is the current turn)
+		assertEquals("endTurn:1", engine.endTurn(game, "false"));
 		
-		engine.drawToken(game);
-		purples = 0;
+		//ending a turn with the withdrawal of index 1 (which is the current turn)
+		assertEquals("endTurn:2", engine.endTurn(game, "true"));
 		
-		for(int i = 0; i < game.getAllPlayers().size(); i++) {
-			if(game.getAllPlayers().get(i).checkToken(0) == true) {
-				purples++;
-			}
-		}
-	}
-	
-	
-	@Test
-	public void twoPlayerStartTournamentTest() {
-		assertEquals(1, purples);
+		//player at index 2 ended their turn normally
+		assertEquals("endTurn:3", engine.endTurn(game, "false"));
 		
+		//player at index 3 withdraws from tournament
+		assertEquals("endTurn:4", engine.endTurn(game, "true"));
 		
+		//player at index 4 ended their turn normally
+		assertEquals("endTurn:0", engine.endTurn(game, "false"));
+		
+		//player at index 0 withdrew from the game
+		assertEquals("endTurn:2", engine.endTurn(game, "true"));
+		
+		//player at index 2 withdrew from the game leaving winning player at index 4
+		assertEquals("endTournament:4", engine.endTurn(game, "true"));
 	}
 }
