42e0017fa70a5cd6249283792b6aa48af16c0c37
AustinMitchell <n.austin.mitchell@gmail.com>
2016-03-03 11:49:53 -0500

    Gamepanel now shows displays and moves some cards to display when played

--- src/ui/panel/GamePanel.java
+++ src/ui/panel/GamePanel.java
@@ -8,14 +8,21 @@ import simple.gui.*;
 import simple.gui.panel.*;
 import simple.gui.scrollbox.ScrollDialogBox;
 import ui.*;
+import ui.panel.overlay.NewTournamentOverlay;
 import ui.panel.overlay.OverlayPanel;
+import ui.utilitypanel.CardDisplayPanel;
+import ui.utilitypanel.StatusBar;
 
 public class GamePanel extends ScaledPanel {
+	public enum OverlayCommand {
+		START_TOURNAMENT, END_TURN
+	}
+	
 	public static final int THIS_PLAYER = 0;
 	
 	GameController controller;
 	
-	CardDisplayPanel[] display;
+	CardDisplayPanel[] hand, display;
 	StatusBar[] playerStatus;
 	ScrollDialogBox messageScrollBox;
 	DescriptionBox descriptionBox;
@@ -23,7 +30,10 @@ public class GamePanel extends ScaledPanel {
 	CardWidget deck, discard;
 	Image emptyDiscard;
 	Button endTurn, exitGame;
+	
 	OverlayPanel currentOverlay;
+	OverlayCommand overlayCommand;
+	boolean startNewOverlay;
 	
 	int numPlayers;
 	
@@ -37,6 +47,7 @@ public class GamePanel extends ScaledPanel {
 	int aiTimer;
 	int aiActions;
 	
+
 	public GamePanel(final int numPlayers, GameController controller) {
 		super();
 		
@@ -49,90 +60,134 @@ public class GamePanel extends ScaledPanel {
 		currentTurn = 0;
 		turnStart = true;
 		
+		// Main panel with hands, displays and draw. Any overlays that come up also get placed here.
 		mainGamePanel = new ScaledPanel(){{			
+			// Set orientations for each hand and display, and whether cards will be faceup or down
+			hand =    new CardDisplayPanel[numPlayers];
 			display = new CardDisplayPanel[numPlayers];
+			hand   [THIS_PLAYER] = new CardDisplayPanel(Image.Orientation.UP, true);
 			display[THIS_PLAYER] = new CardDisplayPanel(Image.Orientation.UP, true);
 			switch(numPlayers) {
 				case 2:
-					display[1] = new CardDisplayPanel(Image.Orientation.UP, false);
+					hand   [1] = new CardDisplayPanel(Image.Orientation.DOWN, false);
+					display[1] = new CardDisplayPanel(Image.Orientation.UP, true);
 					break;
 				case 3:
-					display[1] = new CardDisplayPanel(Image.Orientation.RIGHT, false);
-					display[2] = new CardDisplayPanel(Image.Orientation.LEFT, false);
+					hand   [1] = new CardDisplayPanel(Image.Orientation.RIGHT, false);
+					hand   [2] = new CardDisplayPanel(Image.Orientation.LEFT, false);
+					
+					display[1] = new CardDisplayPanel(Image.Orientation.RIGHT, true);
+					display[2] = new CardDisplayPanel(Image.Orientation.LEFT, true);
 					break;
 				case 4:
-					display[1] = new CardDisplayPanel(Image.Orientation.RIGHT, false);
-					display[2] = new CardDisplayPanel(Image.Orientation.UP, false);
-					display[3] = new CardDisplayPanel(Image.Orientation.LEFT, false);
+					hand   [1] = new CardDisplayPanel(Image.Orientation.RIGHT, false);
+					hand   [2] = new CardDisplayPanel(Image.Orientation.DOWN, false);
+					hand   [3] = new CardDisplayPanel(Image.Orientation.LEFT, false);
+					
+					display[1] = new CardDisplayPanel(Image.Orientation.RIGHT, true);
+					display[2] = new CardDisplayPanel(Image.Orientation.UP, true);
+					display[3] = new CardDisplayPanel(Image.Orientation.LEFT, true);
 					break;
 				case 5:
-					display[1] = new CardDisplayPanel(Image.Orientation.RIGHT, false);
-					display[2] = new CardDisplayPanel(Image.Orientation.UP, false);
-					display[3] = new CardDisplayPanel(Image.Orientation.UP, false);
-					display[4] = new CardDisplayPanel(Image.Orientation.LEFT, false);
+					hand   [1] = new CardDisplayPanel(Image.Orientation.RIGHT, false);
+					hand   [2] = new CardDisplayPanel(Image.Orientation.DOWN, false);
+					hand   [3] = new CardDisplayPanel(Image.Orientation.DOWN, false);
+					hand   [4] = new CardDisplayPanel(Image.Orientation.LEFT, false);
+					
+					display[1] = new CardDisplayPanel(Image.Orientation.RIGHT, true);
+					display[2] = new CardDisplayPanel(Image.Orientation.UP, true);
+					display[3] = new CardDisplayPanel(Image.Orientation.UP, true);
+					display[4] = new CardDisplayPanel(Image.Orientation.LEFT, true);
 					break;
 				default:
 					throw new RuntimeException("Initialized game with a bad number of players (not within 2 an 5)");
 			}
 			
-			for (int d=0; d<display.length; d++) {
+			// TODO: Fix, adds random cards for testing
+			for (int d=0; d<hand.length; d++) {
 				for (int i=0; i<8; i++) {
 					int[] v = CardData.ALL_CARD_VALUES[(int)(Math.random()*CardData.ALL_CARD_VALUES.length)];
 					CardWidget c = new CardWidget(v[0], v[1]);
-					display[d].addCard(c);
+					hand[d].addCard(c);
 				}
 				if (d != THIS_PLAYER) {
-					display[d].setCardMoveOnHover(false);
+					hand[d].setCardMoveOnHover(false);
 				} else {
-					display[d].setEnabled(false);
+					hand[d].setEnabled(false);
 				}
-				display[d].setDrawContainingPanel(false);
+				display[d].setDrawContainingPanel(true);
 				display[d].setFillColor(null);
 			}
 			
+			// Panel to get the deck and discard in the right spot. Panel background is invisible
 			CardDisplayPanel deckPlaceHolder = new CardDisplayPanel(Image.Orientation.UP, false);
 			
+			// Card widget for the deck
 			deck = new CardWidget(0, 0, false);
 			
+			// Same for discard pile
 			discard = new CardWidget(0, 0, false);
 			
 			deckPlaceHolder.addCard(deck);
 			deckPlaceHolder.addCard(discard);
 			
+			// Image to render when discard is empty
 			emptyDiscard = new Image(144, 200);
 			draw.setColors(new Color(220, 220, 220), null);
 			draw.rect(emptyDiscard, 0, 0, 144, 200);
 			discard.setImage(emptyDiscard);
 			discard.setMoveOnHover(false);
 			
+			// Layouts for player displays and hands according to number of players
+			// Represents relative x and y positions on panel, and width and height variables
 			switch(numPlayers) {
 				case 2:
-					addWidget(display[THIS_PLAYER], 17, 75, 66, 23);
-					addWidget(display[1], 17,  2, 66, 23);
+					addWidget(hand[THIS_PLAYER],    17,  75, 66, 23);
+					addWidget(hand[1],              17, -19, 66, 23);
+					
+					addWidget(display[THIS_PLAYER], 17,  50, 66, 23);
+					addWidget(display[1],           17,   6, 66, 23);
 					break;
 				case 3:
-					addWidget(display[THIS_PLAYER], 25, 75, 50, 23);
-					addWidget(display[1],  2, 15, 13, 70);
-					addWidget(display[2], 85, 15, 13, 70);
+					addWidget(hand[THIS_PLAYER],    25,  75, 50, 23);
+					addWidget(hand[1],             -10,  15, 13, 70);
+					addWidget(hand[2],              97,  15, 13, 70);
+					
+					addWidget(display[THIS_PLAYER], 25,  50, 50, 23);
+					addWidget(display[1],            5,  15, 13, 70);
+					addWidget(display[2],           82,  15, 13, 70);
 					break;
 				case 4:
-					addWidget(display[THIS_PLAYER], 25, 75, 50, 23);
-					addWidget(display[1],  2, 15, 13, 70);
-					addWidget(display[2], 25,  2, 50, 23);
-					addWidget(display[3], 85, 15, 13, 70);
+					addWidget(hand[THIS_PLAYER],    25,  75, 50, 23);
+					addWidget(hand[1],             -10,  15, 13, 70);
+					addWidget(hand[2],              25, -19, 50, 23);
+					addWidget(hand[3],              97,  15, 13, 70);
+					
+					addWidget(display[THIS_PLAYER], 25,  50, 50, 23);
+					addWidget(display[1],            5,  15, 13, 70);
+					addWidget(display[2],           25,   6, 50, 23);
+					addWidget(display[3],           82,  15, 13, 70);
 					break;
 				case 5:
-					addWidget(display[THIS_PLAYER], 25, 75, 50, 23);
-					addWidget(display[1],  2, 30, 13, 65);
-					addWidget(display[2],  2,  2, 45, 20);
-					addWidget(display[3], 53,  2, 45, 20);
-					addWidget(display[4], 85, 30, 13, 65);
+					addWidget(hand[THIS_PLAYER],    25,  75, 50, 23);
+					addWidget(hand[1],             -10,  30, 13, 65);
+					addWidget(hand[2],               2, -16, 45, 20);
+					addWidget(hand[3],              53, -16, 45, 20);
+					addWidget(hand[4],              97,  30, 13, 65);
+					
+					addWidget(display[THIS_PLAYER], 25,  50, 50, 23);
+					addWidget(display[1],            5,  30, 13, 65);
+					addWidget(display[2],            2,   6, 45, 20);
+					addWidget(display[3],           53,   6, 45, 20);
+					addWidget(display[4],           82,  30, 13, 65);
 					break;
 			}
 			
-			addWidget(deckPlaceHolder, 0, 40, 100, 20);
+			addWidget(deckPlaceHolder, 0, 32, 100, 14);
 		}};
 		
+		// Panel on the side for other useful widgets like a descriptionbox for cards, end turn button,
+		// tokens collected for each player, statuses, etc.
 		sidePanel = new ScaledPanel() {{
 			setCustomDraw(new CustomDraw() { 
 				public void draw(Widget w) {
@@ -148,7 +203,6 @@ public class GamePanel extends ScaledPanel {
 				playerStatus = new StatusBar[numPlayers];
 				for (int i=0; i<numPlayers; i++) {
 					playerStatus[i] = new StatusBar("Player " + (i+1));
-					playerStatus[i].collectToken(i);
 					addWidget(playerStatus[i], 0, i, 1, 1);
 				}
 			}};
@@ -156,6 +210,7 @@ public class GamePanel extends ScaledPanel {
 			endTurn = new Button("End Turn");
 			endTurn.setEnabled(false);
 			
+			// TODO: remove or modify, here for testing purposes
 			exitGame = new Button("Exit Game");
 			exitGame.setFillColor(Color.RED);
 			exitGame.setBorderColor(Color.WHITE);
@@ -167,11 +222,14 @@ public class GamePanel extends ScaledPanel {
 			addWidget(playerStatusPanel, 3, 62, 94, 23);
 			addWidget(endTurn, 		   20, 87, 60,  8);			
 			addWidget(exitGame, 	   77, 97, 18,  2);
-		}};
-		
+			
+		}};		
 		
 		addWidget(mainGamePanel, 0, 0, 80, 100, 1);
 		addWidget(sidePanel, 	80, 0, 20, 100, 0);
+		
+		// TODO: Remove, here for testing
+		prepareOverlay(OverlayCommand.START_TOURNAMENT);
 		
 	}
 	
@@ -187,6 +245,9 @@ public class GamePanel extends ScaledPanel {
 		handleHand();
 		checkTurnChange();
 		
+		// Handles any overlay stuff
+		handleOverlay();
+		
 		// Handle turns if it's AI's turn
 		handleAITurn();
 		
@@ -195,7 +256,7 @@ public class GamePanel extends ScaledPanel {
 	@Override
 	public void draw() {
 		// Display panel for the current player's turn
-		CardDisplayPanel p = display[currentTurn];
+		CardDisplayPanel p = hand[currentTurn];
 		draw.setColors(null, new Color(0, 200, 0), 4);
 		draw.rect(p.getX(), p.getY(), p.getWidth(), p.getHeight());
 		super.draw();
@@ -209,7 +270,7 @@ public class GamePanel extends ScaledPanel {
 		outer: {
 			if (currentOverlay == null) {
 				for (int i=0; i<numPlayers; i++) {
-					List<Widget> widgetList = display[i].getWidgetList();
+					List<Widget> widgetList = hand[i].getWidgetList();
 					for (int j=widgetList.size()-1; j>=0; j--) {
 						CardWidget c = (CardWidget)widgetList.get(j);
 						if (c.containsMouse()) {
@@ -231,7 +292,7 @@ public class GamePanel extends ScaledPanel {
 	
 	private void checkTurnChange() {
 		if (endTurn.isClicked()) {
-			display[THIS_PLAYER].setEnabled(false);
+			hand[THIS_PLAYER].setEnabled(false);
 			endTurn.setEnabled(false);
 			changeTurn();
 		}
@@ -246,6 +307,7 @@ public class GamePanel extends ScaledPanel {
 		
 		messageScrollBox.addRepeatedTextLine("-");
 		if (currentTurn == THIS_PLAYER) {
+			prepareOverlay(OverlayCommand.START_TOURNAMENT);
 			deck.setEnabled(true);
 			messageScrollBox.addLine("It is now your turn.");
 		} else {
@@ -273,7 +335,7 @@ public class GamePanel extends ScaledPanel {
 	private void drawFromDeck(int player) {
 		turnStart = false;
 		int[] v = CardData.ALL_CARD_VALUES[(int)(Math.random()*CardData.ALL_CARD_VALUES.length)];
-		display[player].addCard(new CardWidget(v[0], v[1]));
+		hand[player].addCard(new CardWidget(v[0], v[1]));
 		if (currentTurn == THIS_PLAYER) {
 			messageScrollBox.addLine(" > You drew a card.");
 		} else {
@@ -283,7 +345,7 @@ public class GamePanel extends ScaledPanel {
 	
 	private void handleDeck() {
 		if (deck.isClicked()) {
-			display[0].setEnabled(true);
+			hand[0].setEnabled(true);
 			deck.setEnabled(false);
 			endTurn.setEnabled(true);
 			drawFromDeck(0);
@@ -292,13 +354,18 @@ public class GamePanel extends ScaledPanel {
 	
 	/** HAND OPERATIONS **/
 	private void playCard(int cardIndex) {
-		CardWidget playedCard = (CardWidget)display[currentTurn].getWidgetList().get(cardIndex);
+		CardWidget playedCard = (CardWidget)(hand[currentTurn].getWidgetList().get(cardIndex));
 		int type = playedCard.getType();
 		int value = playedCard.getValue();
 		
-		discard.setCard(type, value);
-		discard.setFaceUp(true);
-		display[currentTurn].removeIndex(cardIndex);
+		if (type <= 5) {
+			display[currentTurn].addCard(new CardWidget(type, value));
+		} else {
+			discard.setCard(type, value);
+			discard.setFaceUp(true);
+		}
+		
+		hand[currentTurn].removeIndex(cardIndex);
 		
 		if (currentTurn == THIS_PLAYER) {
 			messageScrollBox.addLine(" > You played " + CardData.getCardName(type, value));
@@ -322,13 +389,39 @@ public class GamePanel extends ScaledPanel {
 		if (currentTurn != 0 && aiTimer <= 0) {
 			aiTimer = 45;
 			if (turnStart) {
-				aiActions = (int)Math.min((Math.random()*2)+1, display[currentTurn].getWidgetList().size());
+				aiActions = (int)Math.min((Math.random()*2)+1, hand[currentTurn].getWidgetList().size());
 				drawFromDeck(currentTurn);
 			} else if (aiActions > 0) {
 				aiActions--;
-				playCard((int)(Math.random()*display[currentTurn].numWidgets()));
+				playCard((int)(Math.random()*hand[currentTurn].numWidgets()));
 			} else {
 				changeTurn();
+			}
+		}
+	}
+	
+	private void prepareOverlay(OverlayCommand oc) {
+		startNewOverlay = true;
+		overlayCommand = oc;
+	}
+	private void handleOverlay() {
+		// Checks if a new overlay needs to start
+		if (startNewOverlay) {
+			switch(overlayCommand) {
+				// A new tournament is starting, and you are starting the tournament
+				case START_TOURNAMENT:
+					currentOverlay = new NewTournamentOverlay(descriptionBox, hand[THIS_PLAYER]);
+					break;
+				case END_TURN:
+					break;
+			}
+			startNewOverlay = false;
+			mainGamePanel.addWidget(currentOverlay, 2, 2, 96, 96, 0);
+		// Otherwise, check on the status of the current overlay if one exists
+		} else if (currentOverlay != null) {
+			if (currentOverlay.isOverlayActionComplete()) {
+				mainGamePanel.removeWidget(currentOverlay);
+				currentOverlay = null;
 			}
 		}
 	}
