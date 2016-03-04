dba38a80ad468d11ed1c898f7f2202aa88d46020
Ahmedabdelghani <ahmed.abdelghani.101@gmail.com>
2016-03-03 22:42:48 -0500

    Added functionality for status effects (shield, stun); Also able to check if display has a maiden

--- src/models/Player.java
+++ src/models/Player.java
@@ -8,48 +8,73 @@ public class Player {
 	private String name;
 	private Deck hand;
 	private Deck display;
+	private Deck shieldDeck;
+	private Deck stunDeck;
 	private boolean inTournament;
 	private boolean[] tokens;
-	private boolean[] status;
+	//private boolean[] status;
 	private int drawnToken;
 	
 	public Player(String playerName) {
 		name = playerName;
 		tokens = new boolean[5];
 		for(int i = 0; i < 5; i++) tokens[i] = false;
-		status = new boolean[2];
-		for(int i = 0; i < 2; i++) status[i] = false;
 		hand = new Deck();
 		display = new Deck();
+		shieldDeck = new Deck();
+		stunDeck = new Deck();
 	}
 	
 	public String getName() {
 		return name;
 	}
 	
+	//check if the player is stunned
 	public boolean isStunned() {
-		return status[0];
+		if(stunDeck.deckSize() == 0)
+			return false;
+		
+		else
+			return true;
 	}
 	
-	public void stunPlayer() {
-		status[0] = true;
+	//getStun function (just in case)
+	public Deck getStunDeck()  {
+		return stunDeck;
 	}
 	
-	public void unstunPlayer() {
-		status[0] = false;
-	}
 	
+	//removes the stun from the player but also returns the card in order to throw it in the discard deck
+	/*public Card removeStun() {
+		Card card = stun;
+		stun = null;
+		return card;
+	}*/
+	
+	/*public void shieldPlayer() {
+		stun = new Card(Type.ACTION, Card.SHIELD);
+	}*/
+	
+	//check if player has a shield
 	public boolean hasShield() {
-		return status[1];
-	}
-
-	public void shieldPlayer() {
-		status[1] = true;
+		if(shieldDeck.deckSize() == 0)
+			return false;
+		
+		else
+			return true;
 	}
 	
-	public void unshieldPlayer() {
-		status[1] = false;
+	//getShield function (just in case)
+	public Deck getShieldDeck() {
+		return shieldDeck;
 	}
+	
+	//removes the shield from the player but also returns the card in order to throw it in the discard deck
+	/*public Card removeShield() {
+		Card card = shield;
+		shield = null;
+		return card;
+	}*/
 	
 	public boolean checkToken(int i) {
 		return tokens[i];
@@ -106,6 +131,15 @@ public class Player {
 	
 	public void setDrawnToken(int token) {
 		drawnToken = token;
+	}
+	
+	public boolean displayHasMaiden() {
+		for(int i = 0; i < display.deckSize(); i++) {
+			if(display.getCard(i).getCardType() == Type.WHITE && display.getCard(i).isMaiden()) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public int getDrawnToken() {
