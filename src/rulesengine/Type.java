8e0069a55d8947482beb272553bd5213745166b4
Ahmedabdelghani <ahmed.abdelghani.101@gmail.com>
2016-03-03 22:46:10 -0500

    Changed order of constants

--- src/rulesengine/Type.java
+++ src/rulesengine/Type.java
@@ -3,8 +3,8 @@ package rulesengine;
 public class Type {
 	public static final int PURPLE = 0;
 	public static final int RED = 1;
-	public static final int BLUE = 2;
-	public static final int YELLOW = 3;
+	public static final int YELLOW = 2;
+	public static final int BLUE = 3;
 	public static final int GREEN = 4;
 	public static final int WHITE = 5;
 	public static final int ACTION = 6;
