4cb2d0eaea6f300dd7d6c988dcdb9a69ee2f6019
Ahmedabdelghani <ahmed.abdelghani.101@gmail.com>
2016-03-04 23:31:18 -0500

    completed function for checking playable cards within a hand, also created functions to validate different types of cards

--- src/rulesengine/Validator.java
+++ src/rulesengine/Validator.java
@@ -36,21 +36,88 @@ public class Validator {
 	}
 	
 	
+	
 	public boolean[] cardsAbleToPlay(GameState game) {
 		Deck hand = game.getAllPlayers().get(game.getTurn()).getHand();
-		int tournamentColour = game.getTournamentColour();
+		Card card;
+		boolean displayHasMaiden = game.getAllPlayers().get(game.getTurn()).displayHasMaiden();
 		boolean[] playableCards = new boolean[hand.deckSize()];
 		
 		for(int i = 0; i < hand.deckSize(); i++) {
-			if(hand.getCard(i).getCardType() == game.getTournamentColour() || 
-					hand.getCard(i).getCardType() == Type.WHITE) {
-				playableCards[i] = true;
+			card = hand.getCard(i);
+			
+			if(card.getCardType() != Type.ACTION) {
+				playableCards[i] = validateValueCard(game, card, displayHasMaiden);
+			}
+			
+			else if (card.getCardType() == Type.ACTION && card.getCardValue() == Card.UNHORSE) {
+				playableCards[i] = validateUnhorse(game);
+			}
+			
+			else if (card.getCardType() == Type.ACTION && card.getCardValue() == Card.CHANGE_WEAPON) {
+				playableCards[i] = validateChangeWeapon(game);
+			}
+			
+			else if (card.getCardType() == Type.ACTION && card.getCardValue() == Card.DROP_WEAPON) {
+				playableCards[i] = validateDropWeapon(game);
 			}
 			else {
 				playableCards[i] = false;
 			}
 		}
 		return playableCards;
+	}
+	
+	public boolean validateValueCard(GameState game, Card card, boolean displayHasMaiden) {
+		if(card.getCardType() == game.getTournamentColour()) {
+			return true;
+		}
+		
+		else if (card.getCardType() == Type.WHITE && !card.isMaiden()) {
+			return true;
+		}
+		
+		else if (card.getCardType() == Type.WHITE && card.isMaiden()) {
+			if(displayHasMaiden) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean validateUnhorse(GameState game) {
+		
+		if(game.getTournamentColour() == Type.PURPLE) {
+			return true;
+		}
+		
+		else {
+			return false;
+		}
+	}
+	
+	public boolean validateChangeWeapon(GameState game) {
+		
+		if (game.getTournamentColour() != Type.PURPLE && game.getTournamentColour() != Type.GREEN) {
+			return true;
+		}
+		
+		else {
+			return false;
+		}
+	}
+	
+	public boolean validateDropWeapon(GameState game) {
+		if (game.getTournamentColour() != Type.PURPLE && game.getTournamentColour() != Type.GREEN) {
+			return true;
+		}
+		
+		else {
+			return false;
+		}
 	}
 	
 	
@@ -69,7 +136,7 @@ public class Validator {
 	}
 	
 	
-	public static boolean validateValueCard (GameState game, int cardPos) {
+	/*public static boolean validateValueCard (GameState game, int cardPos) {
 		Card card = game.getAllPlayers().get(game.getTurn()).getHand().getCard(cardPos);
 		int colour = card.getCardType();
 		Player player = game.getAllPlayers().get(game.getTurn());
@@ -83,34 +150,34 @@ public class Validator {
 			return true;
 		}
 		return false;
-	}
+	}*/
 	
 	
 	//Picking the colour can be done in the update engine. Restrict the GUI to only show RED, BLUE, YELLOW
-	public static boolean validateUnhorse(GameState game) {
+	/*public static boolean validateUnhorse(GameState game) {
 		if(game.getTournamentColour() == Type.PURPLE) {
 			return true;
 		}
 		return false;
-	}
+	}*/
 	
 
 	//Picking the colour can be done in the update engine. Restrict the GUI to only show RED, BLUE, YELLOW
 	//and grey out which ever colour that is already the tournament colour 
-	public static boolean validateChangeWeapon(GameState game) {
+	/*public static boolean validateChangeWeapon(GameState game) {
 		if(game.getTournamentColour() == Type.PURPLE || game.getTournamentColour() == Type.GREEN) {
 			return false;
 		}
 		return true;
-	}
+	}*/
 	
 	
-	public static boolean validateDropWeapon(GameState game) {
+	/*public static boolean validateDropWeapon(GameState game) {
 		if(game.getTournamentColour() == Type.PURPLE || game.getTournamentColour() == Type.GREEN) {
 			return false;
 		}
 		return true;
-	}
+	}*/
 	
 	
 	
