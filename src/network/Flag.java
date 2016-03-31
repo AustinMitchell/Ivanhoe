e78e1a49c1fcad6d12fc0d034cb6cf90409216a8
AustinMitchell <n.austin.mitchell@gmail.com>
2016-03-31 15:53:07 -0400

    Added new flags to the flag class

--- src/network/Flag.java
+++ src/network/Flag.java
@@ -4,6 +4,7 @@ public class Flag {
 	public static final String NEW_COM   = "~";
 	public static final String COM_SPLIT = ":";
 	
+	public static final String ACTION_CARD          = "actionCard";
 	public static final String CAN_START_TOURNAMENT = "canStartTournament";
 	public static final String CARD                 = "card";
 	public static final String CARDS_BEGIN          = "cardsBegin";
@@ -13,6 +14,8 @@ public class Flag {
 	public static final String END_TOURNAMENT       = "endTournament";
 	public static final String END_TURN             = "endTurn";
 	public static final String INIT_CLIENT          = "initClient";
+	public static final String IVANHOE_PLAYED       = "ivanhoePlayed";
+	public static final String IVANHOE_RESPONSE     = "ivanhoeResponse";
 	public static final String MAX_PLAYERS          = "maxPlayers";
 	public static final String MAX_PLAYERS_SET      = "maxPlayersSet";
 	public static final String PLAYER_ID            = "playerID";
